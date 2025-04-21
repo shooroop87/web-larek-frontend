20b366c0ef6e2e4399ef5525d1011b5ac08393f4
Andrey <shooroop87@mail.ru>
2025-04-21 17:04:34 +0300

    Update CatalogView.ts

--- src/components/View/CatalogView.ts
+++ src/components/View/CatalogView.ts
@@ -1,36 +1,16 @@
 import { Component } from "../base/Component";
 import { IEvents } from "../base/events";
-import { IProduct } from "../../types";
-import { ProductItemView } from "./ProductItemView";
-import { ProductCollectionModel } from "../Model/ProductCollectionModel";
 
 export class CatalogView extends Component<HTMLElement> {
   constructor(
     container: HTMLElement,
     protected events: IEvents,
-    protected cardTemplate: HTMLTemplateElement,
-    protected model: ProductCollectionModel
+    protected cardTemplate: HTMLTemplateElement
   ) {
     super(container);
-
-    // Подписка на событие от презентера
-    this.events.on("catalog:render", (products: IProduct[]) => {
-      this.renderCatalog(products);
-    });
   }
 
-  renderCatalog(products: IProduct[]) {
-    this.container.innerHTML = "";
-
-    products.forEach((item) => {
-      const card = new ProductItemView(this.cardTemplate, this.events, {
-        onClick: () => {
-          this.model.selectedProduct = item;
-          this.events.emit("modalCard:open", item);
-        }
-      });
-
-      this.container.append(card.render(item));
-    });
+  renderCatalog(cards: HTMLElement[]) {
+    this.container.replaceChildren(...cards);
   }
 }
